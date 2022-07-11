f3c15a1dc659e32bcb59e09ca2a4c772927c06c6
tz337 <tz337@cornell.edu>
2022-07-11 10:52:26 -0400

    fixing js effect

--- js/navscript.js
+++ js/navscript.js
@@ -1,5 +1,10 @@
 
-document.body.classList.add('fade');
+if (document.body.classList.contains('fade')) {
+  document.body.classList.remove('fade')
+} else {
+  document.body.classList.add('fade');
+}
+
 const activePg = window.location.pathname;
 const navLinks = document.querySelectorAll('nav a').forEach(link => {
   if (link.href.includes(`${activePg}`)) { link.classList.add('active') }
